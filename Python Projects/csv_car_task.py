2085149d0c46485096f10b84d46e762ace1580d0
Steven Smith <146367991+StevieJSmith@users.noreply.github.com>
2023-09-29 16:40:32 +0100

    Update csv_car_task.py

--- Python Projects/csv_car_task.py
+++ Python Projects/csv_car_task.py
@@ -2,14 +2,14 @@ import csv
 from collections import namedtuple
 
 cars = []
-car = namedtuple("car", "model year price transmission mileage fuelType tax mpg engineSize")
+car = namedtuple("car", "model year price transmission mileage fuelType tax mpg engineSize") # use namedtuple to structure the format of each car by giving each element a unique key
 
 with open('vw.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, skipinitialspace=True)
-    next(reader)
+    reader = csv.reader(csvfile, skipinitialspace=True) # obtain file content as reader
+    next(reader) # skip over the first row (only column names within)
     for row in reader:
-        new_car = car(*row)
-        cars.append(new_car)
+        new_car = car(*row) # map the whole row of data to the keys provided by namedtuple
+        cars.append(new_car) # add to the cars array
 
 most_expensive = cars[0]
 golf_total = 0
@@ -17,13 +17,13 @@ golf_amount = 0
 polo_mileage_total = 0
 polo_amount = 0
 
-for car in cars:
+for car in cars: # car equals current element 
     if int(car.price) > int(most_expensive.price):
-        most_expensive = car
-    if car.model == "Golf":
+        most_expensive = car # compare all cars to find the most expensive 
+    if car.model == "Golf": # carry out the following if the model of the car is a 'Golf'
         golf_total += int(car.price)
         golf_amount += 1
-    if car.model == "Polo" and car.year == "2020":
+    if car.model == "Polo" and car.year == "2020": # carry out the following if the model is a 'Polo' and manufacturing year is '2020'
         polo_mileage_total += int(car.mileage)
         polo_amount += 1
 
